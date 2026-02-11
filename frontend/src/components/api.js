9e54c25cedc9fdd7d4233cf7c83ed4ef6d71a032
mukul9545 <mukul.yadav.9545@gmail.com>
2026-02-11 14:02:47 +0530

    8th commmit

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,5 +1,5 @@
 // frontend/src/api.js
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = {
   // Employee endpoints
